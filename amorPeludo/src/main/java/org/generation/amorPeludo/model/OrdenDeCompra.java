039347431b5f7c6635dae9c9e90df0ebfca6f31f
FranciscoNunezAlonso <francisco.nunez.alonso@outlook.com>
2023-08-23 16:11:20 -0600

    Se inicia a realizar el CRUD de las órdenes de compra.

--- amorPeludo/src/main/java/org/generation/amorPeludo/model/OrdenDeCompra.java
+++ amorPeludo/src/main/java/org/generation/amorPeludo/model/OrdenDeCompra.java
@@ -1,5 +1,73 @@
 package org.generation.amorPeludo.model;
 
+import java.sql.Date;
+
 public class OrdenDeCompra {
 
-}
+	private long id;
+	private long usuario_id;
+	private long productos_id;
+	private Date fecha;
+	private Double total;
+	private static long totalOrden = 0;
+	
+	public OrdenDeCompra(long usuario_id, long productos_id, Date fecha, Double total) {
+		super();
+		this.usuario_id = usuario_id;
+		this.productos_id = productos_id;
+		this.fecha = fecha;
+		this.total = total;
+		
+		OrdenDeCompra.totalOrden++;
+		this.id=OrdenDeCompra.totalOrden;
+	}//constructor OrdenDeCompra
+
+	public OrdenDeCompra() {
+		super();
+		OrdenDeCompra.totalOrden++;
+		this.id=OrdenDeCompra.totalOrden;
+	}//constructor vacío OrdenCompra
+
+	public long getUsuario_id() {
+		return usuario_id;
+	}// getUsuario
+
+	public void setUsuario_id(long usuario_id) {
+		this.usuario_id = usuario_id;
+	}//setUsuario
+
+	public long getProductos_id() {
+		return productos_id;
+	}//getProductos
+
+	public void setProductos_id(long productos_id) {
+		this.productos_id = productos_id;
+	}//setProductos
+
+	public Date getFecha() {
+		return fecha;
+	}//getFecha
+
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}//setFecha
+
+	public Double getTotal() {
+		return total;
+	}//getTotal
+
+	public void setTotal(Double total) {
+		this.total = total;
+	}//setTotal
+
+	public long getId() {
+		return id;
+	}//getId
+
+	@Override
+	public String toString() {
+		return "OrdenDeCompra [id=" + id + ", usuario_id=" + usuario_id + ", productos_id=" + productos_id + ", fecha="
+				+ fecha + ", total=" + total + "]";
+	}//toString
+	
+}//class OrdenDeCompra
--- amorPeludo/src/main/java/org/generation/amorPeludo/service/OrdenDeCompraService.java
+++ amorPeludo/src/main/java/org/generation/amorPeludo/service/OrdenDeCompraService.java
@@ -1,5 +1,75 @@
 package org.generation.amorPeludo.service;
 
-public class OrdenDeCompraService {
+import java.util.ArrayList;
+import java.util.List;
 
-}
+import org.generation.amorPeludo.model.OrdenDeCompra;
+import org.generation.amorPeludo.model.Productos;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrdenDeCompraService {
+	public final ArrayList<OrdenDeCompra> lista = new ArrayList<>();
+
+	@Autowired
+	public OrdenDeCompraService() {
+		lista.add(new OrdenDeCompra("2023-06-15", "%Y-%m-%d", 130.00));	// +++++++++++++++++++++++++++++++++++++ AQUÍ VAMOS +++++++++++++++++++++++++++++++++++++++++++++++++++++
+		
+	} // Constructor OrdenDeCompraService
+
+	public List<OrdenDeCompra> getAllProductos() {
+		return this.lista;
+	} // getAllProductos
+
+	public Productos getProducto(Long id) {
+		Productos tmp = null;
+		for (Productos producto : lista) {
+			if (producto.getId().equals(id)) {
+				tmp = producto;
+				break;
+			} // if
+		} // foreach
+		return tmp;
+	} // getProducto
+
+	public Productos delteProducto(Long id) {
+		Productos tmp = null;
+		for (Productos producto : lista) {
+			if (producto.getId().equals(id)) {
+				tmp = lista.remove(lista.indexOf(producto));
+				break;
+			} // if
+		} // foreach
+		return tmp;
+	} // deleteProducto
+
+	public Productos addProducto(Productos producto) {
+		lista.add(producto);
+		return null;
+	} // addProducto
+
+	public Productos updateProducto(Long id, String tittle, Double price, String description, String img1,
+			String img2) {
+
+		Productos tmp = null;
+		for (Productos producto : lista) {
+			if (producto.getId().equals(id)) {
+				if (tittle != null)
+					producto.setTittle(tittle);
+				if (price != null)
+					producto.setPrice(price);
+				if (description != null)
+					producto.setDescription(description);
+				if (img1 != null)
+					producto.setImg1(img1);
+				if (img2 != null)
+					producto.setImg2(img2);
+				tmp = producto;
+				break;
+			} // if
+		} // foreach
+		return tmp;
+	}
+
+}// class OrdenDeCompraService
